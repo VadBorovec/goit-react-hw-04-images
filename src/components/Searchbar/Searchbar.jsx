c2d8c96a783cd7b293479944c589663ca0df1a70
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-06-28 16:12:32 +0300

    Update Searchbar.jsx
    from class to func component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,47 +1,93 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { ImSearch } from 'react-icons/im';
+import Notiflix from 'notiflix';
+
 import './Searchbar.css';
 
-class Searchbar extends Component {
-  state = {
-    searchQuery: '',
-  };
+// !======functional component
 
-  handleSubmit = e => {
+const Searchbar = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSubmit = e => {
     e.preventDefault();
-    const { searchQuery } = this.state;
-    const { onSubmit } = this.props;
+
+    if (searchQuery.trim() === '') {
+      Notiflix.Notify.failure('Please enter a search query.');
+      return;
+    }
+
     onSubmit(searchQuery);
-    this.setState({ searchQuery: '' });
+    setSearchQuery('');
   };
 
-  handleChange = e => {
-    this.setState({ searchQuery: e.target.value });
+  const handleChange = e => {
+    setSearchQuery(e.target.value.toLowerCase());
   };
 
-  render() {
-    const { searchQuery } = this.state;
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit}>
+        <button type="submit" className="SearchForm-button">
+          <ImSearch className="SearchForm-button-icon" />
+        </button>
 
-    return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
-          <button type="submit" className="SearchForm-button">
-            <ImSearch className="SearchForm-button-icon" />
-          </button>
+        <input
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchQuery}
+          onChange={handleChange}
+        />
+      </form>
+    </header>
+  );
+};
 
-          <input
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={searchQuery}
-            onChange={this.handleChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+// !======class
+
+// class Searchbar extends Component {
+//   state = {
+//     searchQuery: '',
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { searchQuery } = this.state;
+//     const { onSubmit } = this.props;
+//     onSubmit(searchQuery);
+//     this.setState({ searchQuery: '' });
+//   };
+
+//   handleChange = e => {
+//     this.setState({ searchQuery: e.target.value });
+//   };
+
+//   render() {
+//     const { searchQuery } = this.state;
+
+//     return (
+//       <header className="Searchbar">
+//         <form className="SearchForm" onSubmit={this.handleSubmit}>
+//           <button type="submit" className="SearchForm-button">
+//             <ImSearch className="SearchForm-button-icon" />
+//           </button>
+
+//           <input
+//             className="SearchForm-input"
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={searchQuery}
+//             onChange={this.handleChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
 
 export default Searchbar;
