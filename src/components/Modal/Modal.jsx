0e449942bd3a92402f3928b6927a229e915a30df
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-06-28 16:33:49 +0300

    Update Modal.jsx
    from class to func component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,75 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
-
 import './Modal.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+// !======functional component
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+const Modal = ({ largeImageURL, tagImage, onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
 
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
 
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { largeImageURL, tagImage } = this.props;
-    return createPortal(
-      <div className="Overlay" onClick={this.handleBackdropClick}>
-        <div className="Modal">
-          <img src={largeImageURL} alt={tagImage} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div className="Overlay" onClick={handleBackdropClick}>
+      <div className="Modal">
+        <img src={largeImageURL} alt={tagImage} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+// !======class
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { largeImageURL, tagImage } = this.props;
+//     return createPortal(
+//       <div className="Overlay" onClick={this.handleBackdropClick}>
+//         <div className="Modal">
+//           <img src={largeImageURL} alt={tagImage} />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
 
 export default Modal;
